01986921cf42714622d5f3c476572a185ad27dca
msapna717 <sapna.mishra@sourcebits.com>
2020-01-22 12:36:04 +0530

    Menu scenerio - updated some packages paths and simulation(incrementUsersPerSec)

--- src/test/scala/com/spn/requests/GetMenuRequest.scala
+++ src/test/scala/com/spn/requests/GetMenuRequest.scala
@@ -1,9 +1,10 @@
-package test.scala.com.spn.requests
+package com.spn.requests
 
 import com.spn.config.Config
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+//"Get Menu" Request
 object GetMenuRequest {
   val getMenu= http("Get Menu")
     .get(Config.app_url + Config.GET_MENU_URL)
