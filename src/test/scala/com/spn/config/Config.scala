fc7c096f390b2d57f4bba87bc2e3313b97222ca2
Yokesh <yokesh.kuamr@sourcebits.com>
2020-01-23 18:31:23 +0530

    login API

--- src/test/scala/com/spn/config/Config.scala
+++ src/test/scala/com/spn/config/Config.scala
@@ -8,16 +8,16 @@ object Config {
 
   //api urls
   val URL_INITIAL_CONFIG = "${tenant}/1.4/${cluster}/${locale}/${channel}/${propertyName}/INITIAL/CONFIG?"
-  val GET_MENU_URL="/${tenant}/1.4/${cluster}/${locale}/${channel}/${propertyName}/MENU/menu";
-
+  val GET_MENU_URL= "${tenant}/1.4/${cluster}/${locale}/${channel}/${propertyName}/MENU/menu"
+  val Login_URL= "${tenant}/1.4/${cluster}/${locale}/${channel}/${propertyName}/LOGIN"
   val users = Integer.getInteger("users", 1).toInt
   val rampUp = Integer.getInteger("rampup", 300).toInt
   val duration = Integer.getInteger("duration",300).toInt
   val throughput = Integer.getInteger("throughput", 1).toInt
-  val times = Integer.getInteger("times", 5).toInt
-  val eachLevelLasting = Integer.getInteger("eachLevelLasting", 5).toInt
-  val separatedByRampsLasting = Integer.getInteger("separatedByRampsLasting", 5).toInt
-  val startingFrom = Integer.getInteger("startingFrom", 5).toInt
+  val times = Integer.getInteger("times", 1).toInt
+  val eachLevelLasting = Integer.getInteger("eachLevelLasting", 1).toInt
+  val separatedByRampsLasting = Integer.getInteger("separatedByRampsLasting", 1).toInt
+  val startingFrom = Integer.getInteger("startingFrom", 1).toInt
   val defaultResponseTime=Integer.getInteger("responseTime", 1).toInt
 
   //http protocol configuration
@@ -25,6 +25,7 @@ object Config {
     .baseUrl(app_url )
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .contentTypeHeader("application/json")
 
 }
 
