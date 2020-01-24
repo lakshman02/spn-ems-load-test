654cdad49718c81f7fe13e32525e8415bfaa4922
msapna717 <sapna.mishra@sourcebits.com>
2020-01-24 10:59:45 +0530

    Committing Config

--- src/test/scala/com/spn/config/Config.scala
+++ src/test/scala/com/spn/config/Config.scala
@@ -29,7 +29,7 @@ object Config {
 
   //http protocol configuration
   val httpProtocol = http
-    .baseUrl(app_url )
+    .baseUrl(app_url)
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
     .contentTypeHeader("application/json")
