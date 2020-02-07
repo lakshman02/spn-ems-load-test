4ed66efcb50b94904862341574283d99d8c9435e
sajesh.kayyath <sajesh.kayyath@sourcebits.com>
2020-02-07 15:00:30 +0530

    restful header attr

--- src/test/scala/com/spn/config/Config.scala
+++ src/test/scala/com/spn/config/Config.scala
@@ -49,7 +49,7 @@ object Config {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
     .contentTypeHeader("application/json")
-
+    .header("restful","yes")
 
 }
 
