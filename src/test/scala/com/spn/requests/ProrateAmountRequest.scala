6721fc2587e4a710f8d0a143258a60ec76034246
Karan <karan.rai@sourcebits.com>
2020-02-19 17:01:25 +0530

    Started working on prorate amount API

---
+++ src/test/scala/com/spn/requests/ProrateAmountRequest.scala
@@ -0,0 +1,23 @@
+package com.spn.requests
+
+import com.spn.config.Config
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object ProrateAmountRequest {
+
+  val otpHeader = Map("x-via-device" -> "true")
+
+  val prorateAmountRequest = exec(http("Create OTP Request")
+    .post(Config.app_url + Config.CREATE_OTP_URL)
+    .headers(otpHeader)
+    .body(StringBody("""{
+                       |"channelPartnerID": "${channelPartnerID}",
+                       |"mobileNumber": "${evg_phone_number}",
+                       |"country": "${country}",
+                       |"timestamp": "${getDateTime}"
+                       |}""".stripMargin)).asJson
+    .check(status is 200)
+    .check(jsonPath("$.resultCode").is("OK")))
+
+}
