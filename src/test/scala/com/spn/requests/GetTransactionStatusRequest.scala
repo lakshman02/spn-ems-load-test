b3cdb1edbfde51459a245d84951cf54fbcee4881
sajesh.kayyath <sajesh.kayyath@sourcebits.com>
2020-02-19 15:00:27 +0530

    modified old apis

--- src/test/scala/com/spn/requests/GetTransactionStatusRequest.scala
+++ src/test/scala/com/spn/requests/GetTransactionStatusRequest.scala
@@ -16,9 +16,9 @@ object GetTransactionStatusRequest {
     .headers(Map("Authorization" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyMDAyMDYwNjM5MTAxNTczNiIsInRva2VuIjoiSFlYdC1tSXRyLTJOeVUtZ1VCQi1qYld0LUx4d1AtZU0iLCJleHBpcmF0aW9uTW9tZW50IjoiMjAyMS0wMi0wNVQwNjo0MTo0OS45MjVaIiwiaXNQcm9maWxlQ29tcGxldGUiOmZhbHNlLCJzZXNzaW9uQ3JlYXRpb25UaW1lIjoiMjAyMC0wMi0wNlQwNjo0MTo0OS45MjdaIiwiY2hhbm5lbFBhcnRuZXJJRCI6Ik1TTUlORCIsImZpcnN0TmFtZSI6IiIsIm1vYmlsZU51bWJlciI6IiIsImRhdGVPZkJpcnRoIjoiIiwiZ2VuZGVyIjoiIiwicHJvZmlsZVBpYyI6IiIsInNvY2lhbFByb2ZpbGVQaWMiOiIiLCJzb2NpYWxMb2dpbklEIjpudWxsLCJzb2NpYWxMb2dpblR5cGUiOm51bGwsImlzRW1haWxWZXJpZmllZCI6ZmFsc2UsImlzTW9iaWxlVmVyaWZpZWQiOnRydWUsImxhc3ROYW1lIjoiIiwiZW1haWwiOiIiLCJpYXQiOjE1ODA5NzEzMTAsImV4cCI6MTU4MTU3MDk1MH0.dLrvkE3NJc-jlCrLSYZXh1Xi113QnZSYEBPnaY09GFk",
       "x-via-device" -> "true"))
     .body(StringBody("""{
-                       "channelPartnerID": "MSMIND1",
+                       "channelPartnerID": "${channelPartnerID}",
                        "transactionId": "qr_EAB92TPTpAdUdd",
-                       "paymentChannel":"BHARATQR",
+                       "paymentChannel":"${paymentChannel}",
                        }""")).asJson
     .check(status is 200)
     .check(jsonPath("$.resultCode").is("OK"))
--- src/test/scala/com/spn/simulations/SPNAPISimulation.scala
+++ src/test/scala/com/spn/simulations/SPNAPISimulation.scala
@@ -14,7 +14,9 @@ class SPNAPISimulation extends Simulation {
 
   val filePath = System.getProperty("filePath")
 
- val source: BufferedSource = Source.fromURL(filePath)
+// val source: BufferedSource = Source.fromURL(filePath)
+
+  val source: BufferedSource = Source.fromFile(filePath)
 
 
   val rawTestList = source.mkString
