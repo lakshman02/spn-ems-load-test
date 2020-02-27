5e2a046de679a79b36c4b5fe5ef1e8cf8b22f662
sajesh.kayyath <sajesh.kayyath@sourcebits.com>
2020-02-27 17:22:33 +0530

    commiting Next API

---
+++ src/test/scala/com/spn/requests/NextRequest.scala
@@ -0,0 +1,17 @@
+package com.spn.requests
+
+import com.spn.config.Config
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object NextRequest {
+
+  val nextRequest = exec(http("Next Request")
+    .get(Config.app_url + Config.NEXT_URL)
+    .headers(Map("Authorization" -> "${RESP_AUTH_TOKEN}"
+      ))
+    .check(status is 200)
+    .check(jsonPath("$.resultCode").is("OK"))
+  )
+
+}
