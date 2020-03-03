600b4f68145ec1afbea2bfb93aedb24d077d54ba
Karan <karan.rai@sourcebits.com>
2020-03-03 17:12:59 +0530

    Removed import statement

--- src/test/scala/com/spn/scenarios/GroupOfBundlesScenario.scala
+++ src/test/scala/com/spn/scenarios/GroupOfBundlesScenario.scala
@@ -1,7 +1,6 @@
 package com.spn.scenarios
 
 import com.spn.requests.GroupOfBundlesRequest
-import com.spn.scenarios.CreateOTPScenario.{dataFeederChannel, dataFeederCluster, dataFeederLocale, dataFeederProperty, dataFeederTenant}
 import io.gatling.core.Predef.{scenario, _}
 
 object GroupOfBundlesScenario{
