3e2febadee702d637e42e49f761a83dbf544959e
RaymondAndrews <raymond.andrews@sourcebits.com>
2020-03-03 19:25:13 +0530

    refactored the feed file

--- src/test/scala/com/spn/scenarios/AddXdrScenario.scala
+++ src/test/scala/com/spn/scenarios/AddXdrScenario.scala
@@ -2,30 +2,23 @@ package com.spn.scenarios
 
 import java.time.LocalDateTime
 
+import com.spn.common.CommonFeedFiles
 import com.spn.requests.AddXdrRequest
 import io.gatling.core.Predef.{scenario, _}
 
 object AddXdrScenario {
 
-  val dataFeederChannel = csv("data/channel.csv").circular
-  val dataFeederCluster = csv("data/cluster.csv").circular
-  val dataFeederLocale = csv("data/locale.csv").circular
-  val dataFeederProperty = csv("data/property.csv").circular
-  val dataFeederTenant = csv("data/tenant.csv").circular
   val contentIdData = csv("data/contentID.csv").circular
   val userCredentials = csv("data/evergent/usersWithAuthtoken.csv.gz").unzip.shard.random
 
-  val dateTimeFeeder = Iterator.continually(
-    Map("getDateTime" -> LocalDateTime.now())
-  )
 
   val addXdrScenario = scenario("Add XDR Scenario")
-    .feed(dataFeederTenant)
-    .feed(dataFeederCluster)
-    .feed(dataFeederLocale)
-    .feed(dataFeederChannel)
-    .feed(dataFeederProperty)
-    .feed(dateTimeFeeder)
+    .feed(CommonFeedFiles.dataFeederChannel)
+    .feed(CommonFeedFiles.dataFeederLocale)
+    .feed(CommonFeedFiles.dataFeederCluster)
+    .feed(CommonFeedFiles.dataFeederTenant)
+    .feed(CommonFeedFiles.dataFeederProperty)
+    .feed(CommonFeedFiles.dateTimeFeeder)
     .feed(userCredentials)
     .feed(contentIdData)
     .exec(AddXdrRequest.addXdr)
