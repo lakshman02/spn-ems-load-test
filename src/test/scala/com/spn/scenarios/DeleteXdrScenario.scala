d4b293dae5afe6872ccea3933a794f3930a47a98
sajesh.kayyath <sajesh.kayyath@sourcebits.com>
2020-03-03 17:42:06 +0530

    Refactored the code to feed csv from Commonfeedfile

--- src/test/scala/com/spn/scenarios/DeleteXdrScenario.scala
+++ src/test/scala/com/spn/scenarios/DeleteXdrScenario.scala
@@ -1,26 +1,21 @@
 package com.spn.scenarios
 
+import com.spn.common.CommonFeedFiles
 import com.spn.requests.DeleteXdrRequest
 import io.gatling.core.Predef.{scenario, _}
 
 object DeleteXdrScenario {
 
-  val dataFeederChannel = csv("data/channel.csv").circular
-  val dataFeederCluster = csv("data/cluster.csv").circular
-  val dataFeederLocale = csv("data/locale.csv").circular
-  val dataFeederProperty = csv("data/property.csv").circular
-  val dataFeederTenant = csv("data/tenant.csv").circular
   val dataFeederContentId = csv("data/contentID.csv").circular
-  val userCredentials = csv("data/evergent/usersWithAuthtoken.csv.gz").unzip.shard.random
 
   val deleteXdrScenario = scenario("Delete XDR Scenario")
-    .feed(dataFeederTenant)
-    .feed(dataFeederCluster)
-    .feed(dataFeederLocale)
-    .feed(dataFeederChannel)
-    .feed(dataFeederProperty)
+    .feed(CommonFeedFiles.dataFeederTenant)
+    .feed(CommonFeedFiles.dataFeederCluster)
+    .feed(CommonFeedFiles.dataFeederLocale)
+    .feed(CommonFeedFiles.dataFeederChannel)
+    .feed(CommonFeedFiles.dataFeederProperty)
+    .feed(CommonFeedFiles.userAuth1KUsers)
     .feed(dataFeederContentId)
-    .feed(userCredentials)
     .exec(DeleteXdrRequest.deleteXdrRequest)
 
 }
--- src/test/scala/com/spn/scenarios/NextAndPreviousScenario.scala
+++ src/test/scala/com/spn/scenarios/NextAndPreviousScenario.scala
@@ -1,26 +1,21 @@
 package com.spn.scenarios
 
+import com.spn.common.CommonFeedFiles
 import com.spn.requests.NextAndPreviousRequest
-import io.gatling.core.Predef.{scenario,_}
+import io.gatling.core.Predef.{scenario, _}
 
 object NextAndPreviousScenario {
 
-  val dataFeederChannel = csv("data/channel.csv").circular
-  val dataFeederCluster = csv("data/cluster.csv").circular
-  val dataFeederLocale = csv("data/locale.csv").circular
-  val dataFeederProperty = csv("data/property.csv").circular
-  val dataFeederTenant = csv("data/tenant.csv").circular
   val contentFeeder = csv("data/contentID.csv").circular
-  val userCredentials = csv("data/evergent/usersWithAuthtoken.csv.gz").unzip.shard.circular
 
   val nextAndPreviousScenario = scenario("Next and Previous Scenario")
-    .feed(dataFeederTenant)
-    .feed(dataFeederCluster)
-    .feed(dataFeederLocale)
-    .feed(dataFeederChannel)
-    .feed(dataFeederProperty)
+    .feed(CommonFeedFiles.dataFeederTenant)
+    .feed(CommonFeedFiles.dataFeederCluster)
+    .feed(CommonFeedFiles.dataFeederLocale)
+    .feed(CommonFeedFiles.dataFeederChannel)
+    .feed(CommonFeedFiles.dataFeederProperty)
+    .feed(CommonFeedFiles.userAuth1KUsers)
     .feed(contentFeeder)
-    .feed(userCredentials)
     .exec(NextAndPreviousRequest.nextAndPreviousRequest)
 
 }
