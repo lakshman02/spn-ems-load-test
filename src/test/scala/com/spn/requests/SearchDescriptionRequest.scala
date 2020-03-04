6d2e2de69e6154d2ea32b330ff0d572ff224237f
sapna-mishra <sapna.mishra@sourcebits.com>
2020-03-04 14:27:28 +0530

    conflict resolved

--- src/test/scala/com/spn/requests/SearchDescriptionRequest.scala
+++ src/test/scala/com/spn/requests/SearchDescriptionRequest.scala
@@ -8,18 +8,13 @@ object SearchDescriptionRequest {
 
   val searchDescriptionRequest = exec(http("Search Description Request")
     .get(Config.app_url + Config.SEARCH_DESCRIPTION_URL)
-<<<<<<< HEAD
     .headers(Config.sentHeaders)
     .queryParam("query", "king")
     .queryParam("filter_contentType", "VOD")
     .queryParam("maxResults", "20")
-=======
-    .headers(Map("Authorization" -> "${RESP_AUTH_TOKEN}",
-      "x-via-device" -> "true"))
     .queryParam("query", "${query}")
     .queryParam("filter_contentType", "${filter_contentType}")
     .queryParam("maxResults", "${maxResults}")
->>>>>>> eff9e06b088efab2c1f647602cbc395d33c3f120
     .check(status is 200)
     .check(jsonPath("$.resultCode").is("OK"))
   )
