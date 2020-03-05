97fc967d66da6bb637e7c819e03e3cca81b30c94
Karan Rai <karan.rai@sourcebits.local>
2020-03-05 19:09:30 +0530

    Changed userAuth1KUsers csv reading method to circular from random

--- src/test/scala/com/spn/common/CommonFeedFiles.scala
+++ src/test/scala/com/spn/common/CommonFeedFiles.scala
@@ -21,7 +21,7 @@ object CommonFeedFiles {
   val dataFeederServiceIDOnlyDetails = csv("data/service_id_data.csv").circular
 
   // User Specific
-  val userAuth1KUsers = csv("data/evergent/usersWithAuthtoken.csv.gz").unzip.shard.random
+  val userAuth1KUsers = csv("data/evergent/usersWithAuthtoken.csv.gz").unzip.shard.circular
 
   // inputStagingWeb
   val inputStagingDataFeeder=csv("data/inputStagingWeb.csv").circular
