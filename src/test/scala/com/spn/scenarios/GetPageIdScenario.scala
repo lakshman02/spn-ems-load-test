8491539ceeb77aa9266c051bcac43509979b11cc
Sajesh T Kayyath <sajesh.kayyath@sourcebits.com>
2020-03-25 00:16:13 +0530

    Page Group

--- src/test/scala/com/spn/scenarios/GetPageIdScenario.scala
+++ src/test/scala/com/spn/scenarios/GetPageIdScenario.scala
@@ -11,18 +11,19 @@ object GetPageIdScenario {
 
   val dataFeederPageId = csv("data/pageid.csv").circular
 
-  val PageId = scenario("Get Page Id")
+  val PageId = scenario("Get Page Id").group("Get Page Group") {
 
-    .feed(CommonFeedFiles.dataFeederChannel)
-    .feed(CommonFeedFiles.dataFeederLocale)
-    .feed(CommonFeedFiles.dataFeederCluster)
-    .feed(CommonFeedFiles.dataFeederTenant)
-    .feed(CommonFeedFiles.dataFeederProperty)
-    .feed(dataFeederPageId)
-    .exec(ApiSecurity.getToken)
-    .exec(session => session.set("paginationFrom","0").set("paginationTo","5").set("pageSuffix", "Pagination:0-5"))
-    .exec(GetPageIdRequest.PageId)
-    .pause(1, 3 seconds)
-    .exec(session => session.set("paginationFrom","5").set("paginationTo","10").set("pageSuffix", "Pagination:5-10"))
-    .exec(GetPageIdRequest.PageId)
+    feed(CommonFeedFiles.dataFeederChannel)
+      .feed(CommonFeedFiles.dataFeederLocale)
+      .feed(CommonFeedFiles.dataFeederCluster)
+      .feed(CommonFeedFiles.dataFeederTenant)
+      .feed(CommonFeedFiles.dataFeederProperty)
+      .feed(dataFeederPageId)
+      .exec(ApiSecurity.getToken)
+      .exec(session => session.set("paginationFrom","0").set("paginationTo","5").set("pageSuffix", "Pagination:0-5"))
+      .exec(GetPageIdRequest.PageId)
+      .pause(1, 3 seconds)
+      .exec(session => session.set("paginationFrom","5").set("paginationTo","10").set("pageSuffix", "Pagination:5-10"))
+      .exec(GetPageIdRequest.PageId)
+  }
 }
