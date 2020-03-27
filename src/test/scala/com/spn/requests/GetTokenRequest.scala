095ec03c49f67eb6d072d856a23d0a2111354a02
Sajesh T Kayyath <sajesh.kayyath@sourcebits.com>
2020-03-28 00:09:15 +0530

    Intermediate change of getToken to hit origin - will need to be reverted back

--- src/test/scala/com/spn/requests/GetTokenRequest.scala
+++ src/test/scala/com/spn/requests/GetTokenRequest.scala
@@ -9,7 +9,8 @@ import com.spn.common.Constants
 object GetTokenRequest {
 
   val getToken= exec(http("Get Token Request")
-    .get(Config.app_url + Config.GET_TOKEN_URL)
+    // .get(Config.app_url + Config.GET_TOKEN_URL)
+    .get(Config.app_url_origin + Config.GET_TOKEN_URL)
     .check(status is 200)
     .check(jsonPath("$.resultCode").is("OK"))
     .check(jsonPath("$.resultObj").saveAs(Constants.RESP_SECURITY_TOKEN))
