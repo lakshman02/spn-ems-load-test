f67caf78f51639698bf26b828bf81682a876407f
Sajesh T Kayyath <sajesh.kayyath@sourcebits.com>
2020-03-31 23:16:44 +0530

    Random Login

--- src/test/scala/com/spn/scenarios/journey/complete/SonyLivCompleteUserJourney.scala
+++ src/test/scala/com/spn/scenarios/journey/complete/SonyLivCompleteUserJourney.scala
@@ -1,32 +1,33 @@
 package com.spn.scenarios.journey.complete
 
-import com.spn.common.{ApiSecurity, CommonFeedFiles}
+import com.spn.common.{ApiSecurity, CommonFeedFiles, Constants}
 import com.spn.scenarios.groups.{PageDetailScreen, _}
 import io.gatling.core.Predef._
 
 object SonyLivCompleteUserJourney {
 
   val channelFeederOverride = Array(
-//    Map("channel" -> "IPHONE"),
-//    Map("channel" -> "IPAD"),
+    //    Map("channel" -> "IPHONE"),
+    //    Map("channel" -> "IPAD"),
     Map("channel" -> "ANDROID_PHONE"),
-//    Map("channel" -> "ANDROID_TAB"),
-//    Map("channel" -> "APPLE_TV"),
+    //    Map("channel" -> "ANDROID_TAB"),
+    //    Map("channel" -> "APPLE_TV"),
     Map("channel" -> "FIRE_TV"),
-//    Map("channel" -> "SONY_ANDROID_TV"),
-//    Map("channel" -> "XIAOMI_ANDROID_TV"),
-//    Map("channel" -> "JIO_ANDROID_TV"),
-//    Map("channel" -> "SONY_HTML_TV"),
-//    Map("channel" -> "SAMSUNG_HTML_TV"),
-//    Map("channel" -> "JIO_KIOS"),
+    //    Map("channel" -> "SONY_ANDROID_TV"),
+    //    Map("channel" -> "XIAOMI_ANDROID_TV"),
+    //    Map("channel" -> "JIO_ANDROID_TV"),
+    //    Map("channel" -> "SONY_HTML_TV"),
+    //    Map("channel" -> "SAMSUNG_HTML_TV"),
+    //    Map("channel" -> "JIO_KIOS"),
     Map("channel" -> "WEB"),
-//    Map("channel" -> "IOS")
+    //    Map("channel" -> "IOS")
   ).random
 
   val doNavigateToDetailsPage = false
-  val doUserLogin = true
-  val doNavigateToHomePage = true
 
+  private def randomDoLogin: Boolean = {
+    math.random < 0.25 // with a 0.25 probability
+  }
 
   val doSonyLivCompleteUserJourney = scenario("Complete User Journey")
     .feed(CommonFeedFiles.dataFeederTenant)
@@ -37,47 +38,53 @@ object SonyLivCompleteUserJourney {
     .feed(CommonFeedFiles.dataFeederProperty)
 
     .group("Complete User Journey - Channel - ${channel}") {
-     // exec(session => session.set(Constants.REQ_USER_TYPE, Constants.USER_TYPE_GUEST))
+      // exec(session => session.set(Constants.REQ_USER_TYPE, Constants.USER_TYPE_GUEST))
       exec(ApiSecurity.getToken)
+        .exec(session => session.set(Constants.REQ_USER_TYPE, Constants.USER_TYPE_GUEST)) //Defaulting to guest for user session
         .group("Guest User App Launch - Channel - ${channel}") {
           exec(UserAppLaunchScenario.userAppLaunchScenario)
         }
-//        .group("Guest User Home Screen - Channel - ${channel}") {
-//          exec(HomeScreen.guestUserHomeScreenScenario)
-//        }
-        .doIf(doUserLogin) { // TODO - do we need to put random logins?
-          group("Email Login - Channel - ${channel}") {
-            feed(CommonFeedFiles.dateTimeFeeder)
-              .feed(CommonFeedFiles.userAuthForScenarioTestingUsersUsingRandom)
-              .feed(LoginWithEmailGroup.feederDeviceDetails)
-              .feed(CommonFeedFiles.channelPartnerIdAndAppClientId)
-              .feed(LoginWithEmailGroup.dateOfBirthFeeder)
-              .feed(LoginWithEmailGroup.genderFeeder)
-              .feed(LoginWithEmailGroup.pinCodeFeeder)
-              .exec(LoginWithEmailGroup.doLoginWithEmail)
-          }.doIf(doNavigateToHomePage) {
-            group("Logged in User Home Screen - Channel - ${channel}") {
-              exec(HomeScreen.loggedInUserHomeScreenScenario) // TODO - comment some of the api calls inside
+        .doIf(randomDoLogin) { // Doing random Login
+          exec(session => session.set(Constants.REQ_USER_TYPE, Constants.USER_TYPE_LOGGED_IN))
+            .group("Email Login - Channel - ${channel}") {
+              feed(CommonFeedFiles.dateTimeFeeder)
+                .feed(CommonFeedFiles.userAuthForScenarioTestingUsersUsingRandom)
+                .feed(LoginWithEmailGroup.feederDeviceDetails)
+                .feed(CommonFeedFiles.channelPartnerIdAndAppClientId)
+                .feed(LoginWithEmailGroup.dateOfBirthFeeder)
+                .feed(LoginWithEmailGroup.genderFeeder)
+                .feed(LoginWithEmailGroup.pinCodeFeeder)
+                .exec(LoginWithEmailGroup.doLoginWithEmail)
             }
+        }
+        // This is where home navigation is happening - starts
+        .doIfOrElse(session => session(Constants.REQ_USER_TYPE).as[String].equals(Constants.USER_TYPE_LOGGED_IN)) {
+          group("Logged in User Home Screen - Channel - ${channel}") {
+            exec(HomeScreen.loggedInUserHomeScreenScenario)
+          }
+        } {
+          group("Guest User Home Screen - Channel - ${channel}") {
+            exec(HomeScreen.guestUserHomeScreenScenario)
           }
         }
-        .group("Search Functionality - Channel - ${channel}") {
-            doIfOrElse(doUserLogin){
-              randomSwitch(
-                20d -> group("Search Functionality for Logged-In user - Channel - ${channel}"){
-                  feed(CommonFeedFiles.contentFeeder)
-                  .exec(SearchFunctionalityForUserGroup.doSearchForLoggedInUser)
-                }
-              )
-            }
-          {
+        // This is where home navigation is happening - ends
+        // Search functionality starts here
+        .doIfOrElse(session => session(Constants.REQ_USER_TYPE).as[String].equals(Constants.USER_TYPE_LOGGED_IN)) {
+          group("Logged in User Performing search - Channel - ${channel}") {
             randomSwitch(
-              80d -> group("Search Functionality for Guest user - Channel - ${channel}"){
+              20d -> group("Search Functionality - Logged-In user - Channel - ${channel}") {
                 feed(CommonFeedFiles.contentFeeder)
-                .exec(SearchFunctionalityForUserGroup.doSearchForNonLoggedInUser)
+                  .exec(SearchFunctionalityForUserGroup.doSearchForLoggedInUser)
               }
             )
           }
+        } {
+          randomSwitch(
+            80d -> group("Guest User Performing search - Channel - ${channel}") {
+              feed(CommonFeedFiles.contentFeeder)
+                .exec(SearchFunctionalityForUserGroup.doSearchForNonLoggedInUser)
+            }
+          )
         }
         .doIf(doNavigateToDetailsPage) { //TODO - this is in progress - need to conclude
           group("Guest User Page Details - Channel - ${channel}") {
