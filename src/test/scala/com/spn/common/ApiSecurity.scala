8d158daec43f6235432fe7352093fb0faaa1a964
Sajesh T Kayyath <sajesh.kayyath@sourcebits.com>
2020-04-07 16:07:42 +0530

    Avoiding token call for Individual api testing

--- src/test/scala/com/spn/common/ApiSecurity.scala
+++ src/test/scala/com/spn/common/ApiSecurity.scala
@@ -7,13 +7,25 @@ import io.gatling.core.Predef._
 object ApiSecurity {
 
   val getToken = exec(
+
+    /*
+      enableAPISecurity = 1 ; New security token for every virtual user
+      enableAPISecurity = 2 ; Security token will be passed from FrontLine using "singleSecurityToken"
+      enableAPISecurity = 0 ; Dummy security token will be applied
+     */
     doIfOrElse(session => (Config.enableAPISecurity == 1 && !session.contains(Constants.RESP_SECURITY_TOKEN))){
       tryMax(2, "tokenRetry") {
         exec(GetTokenRequest.getToken)
       }.exitHereIfFailed
     }{
-      exec(session => {
-        session.set(Constants.RESP_SECURITY_TOKEN, Constants.RESP_SECURITY_TOKEN_DUMMY_VAL)
-      })
+      doIfOrElse(session => (Config.enableAPISecurity == 2 && !session.contains(Constants.RESP_SECURITY_TOKEN))){
+        exec(session => {
+          session.set(Constants.RESP_SECURITY_TOKEN, Config.singleSecurityToken)
+        })
+      } {
+        exec(session => {
+          session.set(Constants.RESP_SECURITY_TOKEN, Constants.RESP_SECURITY_TOKEN_DUMMY_VAL)
+        })
+      }
     })
 }
